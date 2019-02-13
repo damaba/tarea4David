af03d276beeac9ec9ebe91d8ac94aedce698d362
damaba <xdamabaxhs@yahoo.com>
2019-02-13 14:30:10 +0100

    Añade constructores en clase modelo

--- src/tarea4david/Leyenda.java
+++ src/tarea4david/Leyenda.java
@@ -14,6 +14,20 @@ public class Leyenda {
    private String nombre;
    private String pasiva;
    private String skin;
+
+    public Leyenda() {
+        nombre = "Mirage";
+        pasiva = "Desaparece";
+        skin = "camuflaje";
+    }
+
+    public Leyenda(String nombre, String pasiva, String skin) {
+        this.nombre = nombre;
+        this.pasiva = pasiva;
+        this.skin = skin;
+    }
+    
+    
    
     
 }
