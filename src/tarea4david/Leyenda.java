91c98063ad16ae63be0a1aa38649f23e631ef34f
damaba <xdamabaxhs@yahoo.com>
2019-02-13 14:04:10 +0100

    Añade atributos en clase modelo

---
+++ src/tarea4david/Leyenda.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4david;
+
+/**
+ *
+ * @author David
+ */
+public class Leyenda {
+
+   private String nombre;
+   private String pasiva;
+   private String skin;
+   
+    
+}
